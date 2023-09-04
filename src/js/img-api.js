8af2cdc279749e0a27bc15c7879f04ee9dd6f854
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2023-09-05 00:03:52 +0300

    finish with btn

--- src/js/img-api.js
+++ src/js/img-api.js
@@ -1,38 +1,20 @@
 import axios from 'axios';
-import Notiflix from 'notiflix';
+
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '39176406-c12c047aa6bfb11d5ee548958';
 
-export async function getImg(name) {
+export async function getImg(name, currentPage = 1) {
   const response = await axios.get(`${BASE_URL}`, {
     params: {
       key: API_KEY,
       q: name,
       per_page: 40,
-      page: 1,
+      page: currentPage,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
     },
-  }).catch(function (error) {
-    Notiflix.Notify.failure('ERROR!!! Something went wrong!!!');
-    if (error.response) {
-      // Запит було зроблено, і сервер відповів кодом стану, який 
-      // виходить за межі 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // Запит було зроблено, але відповіді не отримано 
-      // `error.request` - це екземпляр XMLHttpRequest у браузері та екземпляр 
-      // http.ClientRequest у node.js
-      console.log(error.request);
-    } else {
-      // Щось сталося під час налаштування запиту, що викликало помилку
-      console.log('Error', error.message);
-    }
-    console.log(error.config);
   });
   return response;
-}
+};
