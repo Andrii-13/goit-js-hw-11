221b0c3d7e463a3759bf7996be8c0e952a47f4bb
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2023-09-06 01:01:51 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -13,27 +13,34 @@ let lightbox = new SimpleLightbox('.gallery a', {
 
 let queryValue;
 let inputValue;
-
+let totalNumberOfPges = 40;
 let page;
 
 async function handlerSubmit(e) {
-  window.scrollTo(0,0);
+  window.scrollTo(0, 0);
   e.preventDefault();
   inputValue = e.target.searchQuery.value;
-  if (inputValue === '') {
+
+  if (inputValue === '' || inputValue.trim() === '') {
+    refs.galleryEl.innerHTML = '';
+    Notiflix.Notify.info('Your query is empty. Please try again');
     return;
   }
 
   try {
-    const response = await getFetch(inputValue);
+    const response = await getFetch(inputValue, totalNumberOfPges);
     if (response.data.hits.length === 0) {
+      refs.galleryEl.innerHTML = '';
+      // lightScroll();
+      refs.btnLoadMore.classList.add('visually-hidden');
       Notiflix.Notify.info(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
     }
+
     const {
-      data: { hits }
+      data: { hits },
     } = response;
     const gallery = hits.map(
       ({
@@ -61,7 +68,11 @@ async function handlerSubmit(e) {
     Notiflix.Notify.success(
       `Hooray! We found ${response.data.totalHits} images.`
     );
+    // lightScroll();
     refs.btnLoadMore.classList.remove('visually-hidden');
+    if (response.data.totalHits <= totalNumberOfPges) {
+      refs.btnLoadMore.classList.add('visually-hidden');
+    }
     queryValue = inputValue;
   } catch (error) {
     Notiflix.Notify.failure('ERROR!!! Something went wrong!!!');
@@ -72,49 +83,60 @@ refs.btnLoadMore.addEventListener('click', handlerLoadMore);
 
 async function handlerLoadMore(e) {
   page += 1;
-  
+
   try {
-    const response = await getFetch(queryValue, page);
-  const {
-    data: { hits }
-  } = response;
-  const gallery = hits.map(
-    ({
-      webformatURL,
-      largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads
-    }) =>
-      createMarkup(
+    const response = await getFetch(queryValue, totalNumberOfPges, page);
+    const {
+      data: { hits },
+    } = response;
+    const gallery = hits.map(
+      ({
         webformatURL,
         largeImageURL,
         tags,
         likes,
         views,
         comments,
-        downloads
-      )
-  );
-    refs.galleryEl.insertAdjacentHTML('beforeend',gallery.join(''));
-  lightbox.refresh();
-      //    console.log(response.config.params.page);
-      //  console.log(response.data.totalHits);
-      //  console.log(response.config.params.per_page);
-  if (
-    response.config.params.page >=
-    response.data.totalHits / response.config.params.per_page
-  ){
-    refs.btnLoadMore.classList.add('visually-hidden');
-    Notiflix.Notify.info(
-      "We're sorry, but you've reached the end of search results."); 
-  }
+        downloads,
+      }) =>
+        createMarkup(
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads
+        )
+    );
+    refs.galleryEl.insertAdjacentHTML('beforeend', gallery.join(''));
+    lightbox.refresh();
+
+    if (
+      response.config.params.page >=
+      response.data.totalHits / response.config.params.per_page
+    ) {
+      refs.btnLoadMore.classList.add('visually-hidden');
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
   } catch (error) {
     refs.btnLoadMore.classList.add('visually-hidden');
     Notiflix.Notify.info(
       "We're sorry, but you've reached the end of search results."
-    ); 
+    );
   }
 }
+
+
+// function lightScroll() {
+//   const { height: cardHeight } = document
+//     .querySelector('.gallery')
+//     .firstElementChild.getBoundingClientRect();
+
+//   window.scrollBy({
+//     top: cardHeight * 5,
+//     behavior: 'smooth',
+//   });
+// }
--- src/js/img-api.js
+++ src/js/img-api.js
@@ -4,12 +4,12 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '39176406-c12c047aa6bfb11d5ee548958';
 
-export async function getFetch(name, currentPage = 1) {
+export async function getFetch(name, totalNumberOfPges, currentPage = 1 ) {
   const response = await axios.get(`${BASE_URL}`, {
     params: {
       key: API_KEY,
       q: name,
-      per_page: 40,
+      per_page: totalNumberOfPges,
       page: currentPage,
       image_type: 'photo',
       orientation: 'horizontal',
